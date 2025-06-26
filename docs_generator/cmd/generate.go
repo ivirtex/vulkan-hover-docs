3af79b8cda832e73986c6d93c7010e482db4e51f
ivirtex <hjozwiakdx@gmail.com>
2025-06-26 23:20:17 +0200

    Hmm

--- docs_generator/cmd/generate.go
+++ docs_generator/cmd/generate.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"sync"
+	"os"
+	"strings"
 
+	htmltomarkdown "github.com/JohannesKaufmann/html-to-markdown/v2"
+	"github.com/charmbracelet/log"
 	"github.com/gocolly/colly/v2"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	rootCmd.AddCommand(generateCmd)
+
+	generateCmd.Flags().StringP("path", "p", "docs", "Path to directory where to save the generated Markdown files")
+	generateCmd.Flags().BoolP("verbose", "v", false, "Enable verbose logging")
 }
 
 var generateCmd = &cobra.Command{
@@ -19,25 +25,42 @@ var generateCmd = &cobra.Command{
 	Short: "Generate Markdown documentation from provided URL link",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		url := args[0]
-
-		if err := Generate(url); err != nil {
-			cmd.Println("Error generating documentation:", err)
+		// Set up logging
+		verbose, _ := cmd.Flags().GetBool("verbose")
+		if verbose {
+			log.SetLevel(log.DebugLevel)
 		} else {
-			cmd.Println("Documentation generated successfully.")
+			log.SetLevel(log.InfoLevel)
 		}
+
+		// Get path from flags
+		path, _ := cmd.Flags().GetString("path")
+
+		url := RemoveSlashes(args[0])
+		if url == "" {
+			log.Fatal("URL cannot be empty")
+			return
+		}
+
+		pages, err := ExtractPages(url)
+		if err != nil {
+			log.Fatalf("Error extracting pages: %v", err)
+			return
+		}
+
+		totalProcessedPages, totalSize := DownloadAndProcessPages(pages, path)
+		log.Infof("Generated %d/%d pages", totalProcessedPages, len(pages))
+		log.Infof("Total size of generated documentation: %2.f MB", float32(totalSize)/1024.0/1024.0)
 	},
 }
 
 type Page struct {
-	URL     string
-	Content string
-	Err     error
+	URL   string
+	Title string
 }
 
-func Generate(url string) error {
-	var wg sync.WaitGroup
-	ch := make(chan Page)
+func ExtractPages(url string) ([]Page, error) {
+	var pages []Page
 
 	// Create map to store page name to page content
 	c := colly.NewCollector()
@@ -45,66 +68,183 @@ func Generate(url string) error {
 	// Find and visit all links
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
+		sanitizedLink := RemoveSlashes(RemoveParameters(link))
 
-		fmt.Println("Found link:", link)
+		// Filter out links that are not relevant
+		ignoredNames := []string{"", "#", ".", "..", "?"}
+		for _, ignored := range ignoredNames {
+			if sanitizedLink == ignored {
+				log.Infof("Ignoring link: %s", link)
 
-		wg.Add(1)
-		go DownloadPage(url+link, ch, &wg)
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	c.Visit(url)
-
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
-
-	// Gather results into a map
-	contents := make(map[string]string)
-
-	for page := range ch {
-		if page.Err != nil {
-			fmt.Printf("Failed to download %s: %v\n", page.URL, page.Err)
-			continue
+				return
+			}
 		}
 
-		contents[page.URL] = page.Content
-		fmt.Printf("Downloaded %s (%d bytes)\n", page.URL, len(page.Content))
+		log.Debugf("Found link: %s", sanitizedLink)
+		pages = append(pages, Page{
+			URL:   url + "/" + sanitizedLink,
+			Title: RemoveExtension(sanitizedLink),
+		})
+	})
+
+	log.Infof("Analyzing URL: %s", url)
+
+	c.Visit(url)
+	c.Wait()
+
+	if len(pages) == 0 {
+		return nil, fmt.Errorf("no pages found at the provided URL: %s", url)
 	}
 
-	fmt.Println("\n--- Summary ---")
-	for url, content := range contents {
-		fmt.Printf("%s => %d bytes\n", url, len(content))
-	}
+	log.Infof("Found %d pages at %s", len(pages), url)
 
-	if len(contents) == 0 {
-		fmt.Println("No pages were downloaded.")
-		return fmt.Errorf("no pages downloaded")
-	} else {
-		fmt.Println("Total pages downloaded:", len(contents))
-		return nil
-	}
+	return pages, nil
 }
 
-func DownloadPage(url string, ch chan<- Page, wg *sync.WaitGroup) {
-	defer wg.Done()
+const MaxConcurrentDownloads = 30
 
+// Downloads and converts provided pages to Markdown concurrently
+// Returns a map of Page to Markdown content
+func DownloadAndProcessPages(pages []Page, save_path string) (totalProcessedPages, totalSize int) {
+	sem := make(chan struct{}, MaxConcurrentDownloads)
+
+	for _, page := range pages {
+		sem <- struct{}{} // Acquire a token
+		go func(p Page) {
+			defer func() { <-sem }() // Release the token when done
+
+			log.Infof("Processing page: %s", p.Title)
+
+			log.Debugf("Downloading page: %s", p.Title)
+			htmlContent, err := DownloadPage(p.URL)
+			if err != nil {
+				log.Errorf("Error downloading page %s: %v\n", p.URL, err)
+				return
+			}
+
+			log.Debugf("Converting page %s to Markdown", p.Title)
+			mdContent, err := ConvertPage(htmlContent)
+			if err != nil {
+				log.Errorf("Error converting page %s to Markdown: %v\n", p.Title, err)
+				return
+			}
+
+			log.Debugf("Post-processing page %s", p.Title)
+			mdContent, err = PostProcessPage(p, mdContent)
+			if err != nil {
+				log.Errorf("Error post-processing page %s: %v\n", p.Title, err)
+				return
+			}
+
+			log.Debugf("Saving page %s to %s", p.Title, save_path)
+			if err := SavePage(p, mdContent, save_path); err != nil {
+				log.Errorf("Error saving page %s: %v\n", p.Title, err)
+				return
+			}
+
+			totalProcessedPages++
+			totalSize += len(mdContent)
+		}(page)
+	}
+
+	// Wait for all goroutines to finish
+	for range MaxConcurrentDownloads {
+		sem <- struct{}{} // Ensure we release all tokens
+	}
+
+	return
+}
+
+// Downloads the HTML content of the page at the given URL
+func DownloadPage(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		ch <- Page{URL: url, Err: err}
-		return
+		return "", fmt.Errorf("failed to download page: %w", err)
 	}
+
 	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		ch <- Page{URL: url, Err: err}
-		return
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to download page: %s", resp.Status)
 	}
 
-	ch <- Page{URL: url, Content: string(body)}
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read page content: %w", err)
+	}
+
+	return string(content), nil
+}
+
+// Converts HTML content to Markdown
+func ConvertPage(content string) (string, error) {
+	markdown, err := htmltomarkdown.ConvertString(content)
+	if err != nil {
+		return "", fmt.Errorf("failed to convert HTML to Markdown: %w", err)
+	}
+
+	return markdown, nil
+}
+
+// Removes unnecessary content and fixes links in the Markdown content
+func PostProcessPage(page Page, content string) (string, error) {
+	// Remove "Loading... please wait." from the content
+	content = strings.ReplaceAll(content, "Loading… please wait.", "")
+
+	// Prepend main URL to the HTML links like:
+	// regex "\[\w+\]\((\w+.html)\)"
+	content = strings.ReplaceAll(content, "](./", fmt.Sprintf("](%s/", page.URL))
+
+	return content, nil
+}
+
+// Saves the Markdown content to a file
+func SavePage(page Page, content string, path string) error {
+	if path == "" {
+		path = "docs"
+	}
+
+	// Ensure the directory exists
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create directory %s: %w", path, err)
+	}
+
+	// Create a filename based on the page title or URL
+	filename := page.Title + ".md"
+	filePath := fmt.Sprintf("%s/%s", path, filename)
+
+	// Write the content to the file
+	if err := os.WriteFile(filePath, []byte(content), 0644); err != nil {
+		return fmt.Errorf("failed to save page %s: %w", filePath, err)
+	}
+
+	return nil
+}
+
+func RemoveSlashes(str string) string {
+	// Remove any leading or trailing slashes
+	if len(str) > 0 && str[0] == '/' {
+		str = str[1:]
+	}
+
+	if len(str) > 0 && str[len(str)-1] == '/' {
+		str = str[:len(str)-1]
+	}
+
+	return str
+}
+
+func RemoveParameters(str string) string {
+	if questionMark := strings.Index(str, "?"); questionMark != -1 {
+		return str[:questionMark]
+	}
+
+	return str
+}
+
+func RemoveExtension(str string) string {
+	if dot := strings.LastIndex(str, "."); dot != -1 {
+		return str[:dot]
+	}
+
+	return str
 }
